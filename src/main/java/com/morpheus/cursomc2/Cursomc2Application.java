7eb2a0cc772586940730d75c609a584e5e142e56
Jefferson M Silva <jeffersonmedeirosdasilva@gmail.com>
2021-01-28 16:28:11 -0300

    Criação do projeto

---
+++ src/main/java/com/morpheus/cursomc2/Cursomc2Application.java
@@ -0,0 +1,13 @@
+package com.morpheus.cursomc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomc2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cursomc2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/morpheus/cursomc2/Cursomc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morpheus.cursomc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursomc2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
